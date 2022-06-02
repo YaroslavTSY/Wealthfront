d2c69d57cd024af5f1a095878abbe0c402d3761f
YaroslavTSY <is2419227@gmail.com>
2022-06-02 19:06:04 +0300

    start development

---
+++ js/app.js
@@ -0,0 +1,15 @@
+import * as functions from "./modules/functions.js";
+
+functions.isWebp();
+
+// window.onload = function () {
+// 	//Yandex map's imported from map.js
+// 	map
+// }
+
+
+
+
+
+
+

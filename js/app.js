a9138c45a7954a88a4a68b2e54eca8db71e87732
YaroslavTSY <is2419227@gmail.com>
2022-06-03 22:53:38 +0300

    burger-menu

--- js/app.js
+++ js/app.js
@@ -1,7 +1,36 @@
 import * as functions from "./modules/functions.js";
 
 functions.isWebp();
+functions.isMobile;
 
+
+//! Checking Mobile or PC device
+if (functions.isMobile.any()) {
+	document.body.classList.add('_touch');
+	//! Opening second menu (add class "_active" while click on point) 
+	let menuArrows = document.querySelectorAll('.menu__open-link')
+	if (menuArrows.length > 0) {
+		for (let index = 0; index < menuArrows.length; index++) {
+			const menuArrow = menuArrows[index];
+			menuArrow.addEventListener("click", function (e) {
+				menuArrow.classList.toggle('_active');
+			});
+		}
+	}
+} else {
+	document.body.classList.add('_pc');
+}
+
+//! Menu Burger
+let iconMenu = document.querySelector('.menu__icon');
+if (iconMenu) {
+	const menu = document.querySelector('.menu__body');
+	iconMenu.addEventListener('click', function (e) {
+		document.body.classList.toggle('_lock')
+		iconMenu.classList.toggle('_active');
+		menu.classList.toggle('_active');
+	});
+}
 // window.onload = function () {
 // 	//Yandex map's imported from map.js
 // 	map

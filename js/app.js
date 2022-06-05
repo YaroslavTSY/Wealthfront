a19b54baf6f18047ba21fa6dac5311bea70d311a
YaroslavTSY <is2419227@gmail.com>
2022-06-05 15:00:41 +0300

    unlock body

--- js/app.js
+++ js/app.js
@@ -26,7 +26,6 @@ let iconMenu = document.querySelector('.menu__icon');
 if (iconMenu) {
 	const menu = document.querySelector('.menu__body');
 	iconMenu.addEventListener('click', function (e) {
-		document.body.classList.toggle('_lock')
 		iconMenu.classList.toggle('_active');
 		menu.classList.toggle('_active');
 	});
